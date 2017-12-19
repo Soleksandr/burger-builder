9b319161418572e86452ede3f53b3a00a7b12a8b
Oleksandr Sakun <sakun.oleks@gmail.com>
2017-12-19 23:21:50 +0200

    add Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hello world
-      </div>
+      <Layout>
+        <div>Children props for Layout</div>
+      </Layout>
     );
   }
 }
